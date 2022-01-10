958fc337b9a8ff451fe31932a00ca77bfe707649
Steven <steven.camillieri@protonmail.com>
2022-01-10 07:03:21 -0500

    API call

--- src/pages/questions/Questions.js
+++ src/pages/questions/Questions.js
@@ -1,10 +1,32 @@
 import React from "react";
-import './Questions.css'
+import "./Questions.css";
 
-export default function SignUp() {
+export default function Questions() {
+  const [questions, setQuestions] = React.useState();
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data);
+      });
+  }, []);
+
+  console.log(questions);
+
+  function questionsMap() {
+    return questions.map((i) => {
+      return {
+        question: i,
+        answer: i,
+      };
+    });
+  }
+
   return (
-    <div id="signUpContainer">
-      <h1>Sign Up section</h1>
+    <div id="questionsContainer">
+      {questionsMap}
+      <h1>Sign Up {questionsMap} wwdwdwdwd</h1>
     </div>
   );
 }
