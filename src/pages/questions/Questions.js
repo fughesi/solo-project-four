6a9869adec07486f64c9f88aa78e420078b60e18
Steven <steven.camillieri@protonmail.com>
2022-01-11 06:29:39 -0500

    API is getting called, working on map method

--- src/pages/questions/Questions.js
+++ src/pages/questions/Questions.js
@@ -1,32 +1,46 @@
 import React from "react";
+import questionElement from "../../components/questionElement";
 import "./Questions.css";
 
 export default function Questions() {
-  const [questions, setQuestions] = React.useState();
+  // state variables
+  const [questions, setQuestions] = React.useState([]);
+  const [error, setError] = React.useState();
+  const [loading, setLoading] = React.useState(true);
 
+  // API call
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5")
       .then((res) => res.json())
       .then((data) => {
         setQuestions(data);
-      });
+        setLoading(false);
+      })
+      .catch((error) => setError(error));
   }, []);
 
-  console.log(questions);
+  // loading text
+  if (loading) return <h1 id="loading">LOADING...</h1>;
 
-  function questionsMap() {
-    return questions.map((i) => {
-      return {
-        question: i,
-        answer: i,
-      };
-    });
+  console.log(questions.results[0].question);
+
+
+  // make a map of elements from API --- not working!!!
+  function arrayOfQuestions() {
+    const i = questions.results;
+    return (i) =>
+      i.map((x) => {
+        return [<h3>{x.question}</h3>];
+      });
   }
+
+  // if (questions) return {
+  //   <h1>{questions.results[1].difficulty}</h1>;
 
   return (
     <div id="questionsContainer">
-      {questionsMap}
-      <h1>Sign Up {questionsMap} wwdwdwdwd</h1>
+      <h1>Why is this not working? </h1>
+      {questions ? arrayOfQuestions() : loading}
     </div>
   );
 }
